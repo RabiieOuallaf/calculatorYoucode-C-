1e67d62191231fdf78eeb7c5eed79c253619c5e9
RabieOuallafEng <soudakouan@gmail.com>
2022-09-07 17:15:41 +0100

    calc v2.0.0

--- main.c
+++ main.c
@@ -2,35 +2,55 @@
 
 int main() {
 
-    char symbole;
+
+    // vars
+    int mode;
+    char s;
     float FirstNum , SecondNum;
 
-    printf("wich symbole you'll like to use ? : ");
-    scanf("%c", &symbole);
+    printf("Wich mode you want to use ? (Math : 1) (programming : 2)");
+    scanf("%d", &mode);
 
-    printf("What is the numbers you want to calculat ? (Enter two numbers) : ");
-    scanf("%f", &FirstNum);
-    scanf("%f" , &SecondNum);
+    if(mode == 1) {
 
-    switch (symbole)
-    {
-    case '+':
+        printf("wich symbole you'll like to use ? : ");
+        scanf("%s", &s);
 
-        printf("%.1f + %.1f = %.1f" , FirstNum, SecondNum, FirstNum+SecondNum);
-        break;
+        printf("What is the numbers you want to calculat ? (Enter two numbers) : ");
+        scanf("%f %f", &FirstNum, &SecondNum);
 
-    case '-':
-        printf("%.1f - %.1f = %.1f", FirstNum, SecondNum,FirstNum-SecondNum);
-        break;
-    
-    case '*':
-        printf("%.1f * %.1f = %.1f", FirstNum, SecondNum, FirstNum*SecondNum);
-        break;
-    case '/':
-        printf("%.1f / %.1f = %.1f" , FirstNum, SecondNum, FirstNum/SecondNum);
-        break;
-    
-    default:
-        break;
-    };
+        // the calcul
+
+        switch (s)
+        {
+        case '+':
+
+            printf("%.1f + %.1f = %.1f" , FirstNum, SecondNum, FirstNum+SecondNum);
+            break;
+
+        case '-':
+            printf("%.1f - %.1f = %.1f", FirstNum, SecondNum,FirstNum-SecondNum);
+            break;
+        
+        case '*':
+            printf("%.1f * %.1f = %.1f", FirstNum, SecondNum, FirstNum*SecondNum);
+            break;
+        case '/':
+            printf("%.1f / %.1f = %.1f" , FirstNum, SecondNum, FirstNum/SecondNum);
+            break;
+        
+        default:
+            break;
+        };
+
+
+    }else if(mode == 2) {
+        int FirstInt, SecondInt; 
+        printf("\n Warning : THE ONLY AVAILABLE SYMBOLE IS %");
+        printf("\n What is the numbers you want to calculat ? (Enter two numbers) :");
+        scanf("%d %d", &FirstInt, &SecondInt);
+        printf("%d '%' %d = %d", FirstInt , SecondInt, FirstInt % SecondInt);
+
+    };  
+
 };
